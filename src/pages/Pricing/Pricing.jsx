03f4f11f755549c2bfd1d8d9d5f06780a9e87ae1
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2024-04-26 19:19:33 +0530

    update pricing

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -12,6 +12,125 @@ export default function Pricing() {
         setIsActive(!isActive);
         setIsSchool(!isSchool);
     };
+
+    const SchoolPricing = [
+        {
+            id: 1,
+            tag: ["6-Months"],
+            price: "15,000",
+            priceTag: "With GST",
+            priceDis: "Per member, per Year",
+            subDetails: [
+                {
+                    id: 1,
+                    title: "All features",
+                    subDetails: [
+                        "Membership Programs",
+                        "Group Discounts",
+                    ]
+                },
+                {
+                    id: 2,
+                    title: "Includes 15 Free Live Matches",
+                    subDetails: []
+                },
+                {
+                    id: 3,
+                    title: "Early Access to New Content",
+                    subDetails: []
+                },
+            ]
+        },
+        {
+            id: 2,
+            tag: ["per match live stream"],
+            price: 149,
+            priceTag: "with GST",
+            priceDis: "",
+            subDetails: [
+                {
+                    id: 1,
+                    title: "All features",
+                    subDetails: [
+                        "Membership Programs",
+                        "Exclusive Content",
+                        "Group Discounts",
+                        "Customizable Packages",
+                    ]
+                },
+                {
+                    id: 2,
+                    title: "Includes 30 Free Live Matches",
+                    subDetails: []
+                },
+                {
+                    id: 3,
+                    title: "Priority Support",
+                    subDetails: []
+                },
+            ]
+        },
+    ]
+    const GeneralPricing = [
+        {
+            id: 1,
+            tag: [],
+            price: "Free",
+            priceTag: "",
+            priceDis: "",
+            subDetails: [
+                "Tourment creations",
+                "Watch Score",
+                "Start match",
+                "Ad-Free Experience",
+            ]
+        },
+        {
+            id: 2,
+            tag: ["per match live stream"],
+            price: 149,
+            priceTag: "with GST",
+            priceDis: "",
+            subDetails: [
+                "All features",
+                "One license key",
+                "Yearly subcription plan",
+                "One year support & updates",
+                "Per month ad-free: ₹99",
+                "6 month subscription: ₹499",
+                "12 month subscription: ₹999",
+            ]
+        },
+        {
+            id: 3,
+            tag: ["2 cameras", "popular"],
+            price: 199,
+            priceTag: "with GST",
+            priceDis: "Per member, per Month",
+
+            subDetails: [
+                "Per month ad-free: ₹99",
+                "6 month subscription: ₹499",
+                "12 month subscription: ₹999",
+            ]
+        },
+        {
+            id: 4,
+            tag: ["4 cameras"],
+            price: 299,
+            priceTag: "with GST",
+            priceDis: "Per member, per Month",
+            subDetails: [
+                "All features",
+                "One license key",
+                "Yearly subcription plan",
+                "One year support & updates",
+                "Per month ad-free: ₹99",
+                "6 month subscription: ₹499",
+                "12 month subscription: ₹999",
+            ]
+        },
+    ]
     return (
         <Layout>
             <main className="price">
@@ -28,29 +147,53 @@ export default function Pricing() {
                         isSchool
                             ?
                             <div className="school">
-                                <div className="container">
-                                    <div className="wrapper">
-                                        <div className="tags">
-                                            <h4>heading</h4>
+                                {
+                                    GeneralPricing.map((each) => (
+                                        <div key={each.id} className="container">
+                                            <div className="wrapper">
+                                                <div className="tags">
 
+                                                    {
+                                                        each.tag.map((eachTag) => (
+                                                            <h4 key={eachTag}>{eachTag}</h4>
+                                                        ))
+                                                    }
+
+                                                </div>
+                                                {
+                                                    each.price === "Free"
+                                                        ?
+                                                        <div className="amount">
+
+                                                            <h3>{each.price} <span className="gst">{each.priceTag}</span></h3>
+                                                            <p>{each.priceDis}</p>
+                                                        </div>
+                                                        :
+                                                        <div className="amount">
+
+                                                            <h3><span className="symbol">&#8377;</span>{each.price} <span className="gst">{each.priceTag}</span></h3>
+                                                            <p>{each.priceDis}</p>
+                                                        </div>
+                                                }
+
+
+                                                <ul>
+                                                    {
+                                                        each.subDetails.map((eachSub) => (
+
+                                                            <li key={eachSub}><img src={check} alt="" />{eachSub}</li>
+                                                        ))
+                                                    }
+
+                                                </ul>
+                                            </div>
+
+                                            <button>Choose</button>
                                         </div>
+                                    ))
+                                }
 
-                                        <div className="amount">
-                                            <h3><span className="symbol">&#8377;</span>Free <span className="gst">With GST</span></h3>
-                                            <p>Per member, per Month</p>
-                                        </div>
-
-                                        <ul>
-                                            <li><img src={check} alt="" />Tourment creations</li>
-                                            <li><img src={check} alt="" />Watch Score</li>
-                                            <li><img src={check} alt="" />Start match</li>
-                                            <li><img src={check} alt="" />Ad-Free Experience</li>
-                                        </ul>
-                                    </div>
-
-                                    <button>Choose</button>
-                                </div>
-                                <div className="container">
+                                {/* <div className="container">
                                     <div className="wrapper">
                                         <div className="tags">
                                             <h4>heading</h4>
@@ -115,54 +258,72 @@ export default function Pricing() {
                                     </div>
 
                                     <button>Choose</button>
-                                </div>
+                                </div> */}
                             </div>
                             :
                             <div className="general">
 
-                                <div className="container popular">
-                                    <div className="wrapper">
-                                        <div className="tags">
-                                            <h4>heading</h4>
-                                            <h4 className="pop">popular</h4>
+                                {
+                                    SchoolPricing.map((each) => (
+                                        <div key={each.id} className="container">
+                                            <div className="wrapper">
+                                                <div className="tags">
+                                                    {
+
+                                                        each.tag.map((eachTag) => (
+                                                            <h4 key={eachTag}>{eachTag}</h4>
+                                                        ))
+                                                    }
+
+                                                </div>
+
+                                                {
+                                                    each.price === "Free"
+                                                        ?
+                                                        <div className="amount">
+
+                                                            <h3>{each.price} <span className="gst">{each.priceTag}</span></h3>
+                                                            <p>{each.priceDis}</p>
+                                                        </div>
+                                                        :
+                                                        <div className="amount">
+
+                                                            <h3><span className="symbol">&#8377;</span>{each.price} <span className="gst">{each.priceTag}</span></h3>
+                                                            <p>{each.priceDis}</p>
+                                                        </div>
+                                                }
+
+                                                <ul>
+                                                    {
+                                                        each.subDetails.map((eachSub) => (
+
+                                                            <li key={eachSub.id}>
+                                                                <div className="innerDiv">
+                                                                    <img src={check} alt="" />
+                                                                    <p>{eachSub.title}</p>
+                                                                </div>
+                                                                <ul className="innerUl">
+                                                                    {
+                                                                        eachSub.subDetails.map((eachSub) => (
+                                                                            <li className="innerLi" key={eachSub}>{eachSub}</li>
+                                                                        ))
+                                                                    }
+                                                                </ul>
+                                                            </li>
+                                                        ))
+                                                    }
+                                                    {/* <li><img src={check} alt="" />Tourment creations</li>
+                                                    <li><img src={check} alt="" />Watch Score</li>
+                                                    <li><img src={check} alt="" />Start match</li>
+                                                    <li><img src={check} alt="" />Ad-Free Experience</li> */}
+                                                </ul>
+                                            </div>
+
+                                            <button>Choose</button>
                                         </div>
-                                        <div className="amount">
-                                            <h3><span className="symbol">&#8377;</span>Free <span className="gst">With GST</span></h3>
-                                            <p>Per member, per Month</p>
-                                        </div>
+                                    ))
+                                }
 
-                                        <ul>
-                                            <li><img src={check} alt="" />Tourment creations</li>
-                                            <li><img src={check} alt="" />Watch Score</li>
-                                            <li><img src={check} alt="" />Start match</li>
-                                            <li><img src={check} alt="" />Ad-Free Experience</li>
-                                        </ul>
-                                    </div>
-
-                                    <button>Choose</button>
-                                </div>
-                                <div className="container">
-                                    <div className="wrapper">
-                                        <div className="tags">
-                                            <h4>heading</h4>
-
-                                        </div>
-
-                                        <div className="amount">
-                                            <h3><span className="symbol">&#8377;</span>Free <span className="gst">With GST</span></h3>
-                                            <p>Per member, per Month</p>
-                                        </div>
-
-                                        <ul>
-                                            <li><img src={check} alt="" />Tourment creations</li>
-                                            <li><img src={check} alt="" />Watch Score</li>
-                                            <li><img src={check} alt="" />Start match</li>
-                                            <li><img src={check} alt="" />Ad-Free Experience</li>
-                                        </ul>
-                                    </div>
-
-                                    <button>Choose</button>
-                                </div>
                             </div>
                     }
 
