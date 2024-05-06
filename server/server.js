8f9c1acecdc1d144fe33a774bad97ff0b515c531
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2024-05-06 23:44:02 +0530

    updated server code

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 const disPath = path.join(__dirname, "/dist");
 
 app.use(express.static(disPath));
 
-app.get("/*", (res, req) => {
-    res.sendFile('index.html', { root: disPath })
-})
+app.get("/*", (req, res) => {
+    res.sendFile('index.html', { root: disPath });
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`); 
-})
+    console.log(`server is running on ${PORT}`);
+});
