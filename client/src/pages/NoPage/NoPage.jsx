506702109451fe301416507d2bc4b965bcc85c8d
Zubair wani <48851176+ZubairWani@users.noreply.github.com>
2024-05-07 15:08:16 +0530

    added Nopage

--- client/src/pages/NoPage/NoPage.jsx
+++ client/src/pages/NoPage/NoPage.jsx
@@ -1,7 +1,19 @@
 import "./NoPage.css"
+import img from "../../images/NoPage.png"
+import { useNavigate } from "react-router-dom";
 
 export const NoPage = () => {
+    const navigate = useNavigate();
     return (
-        <div>NoPage</div>
+        
+        
+        <div className="noPage">
+            <img src={img} alt="" />
+            <p>Sorry, This page does not exist</p>
+            <button onClick={() => navigate(-1)}>Go Back</button>
+            
+            
+        </div>
+       
     )
 }
